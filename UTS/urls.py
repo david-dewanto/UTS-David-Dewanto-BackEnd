8784a1875bccf17f490e55193da2688a56bed1bc
David Dewanto <david.dewanto@gmail.com>
2024-11-08 21:33:15 +0700

    update

--- UTS/urls.py
+++ UTS/urls.py
@@ -20,6 +20,6 @@ import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sample/',include('sample.urls')),
+    path('user/',include('sample.user')),
     path('__debug__/',include(debug_toolbar.urls))
 ]
