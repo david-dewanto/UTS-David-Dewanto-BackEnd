f80f5021b3157ee2d9dce6f0bba6f836f7ecdae2
David Dewanto <david.dewanto@gmail.com>
2024-11-08 22:26:22 +0700

    update

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def hello_world(request):
+    data = {
+        "message": "Hello from sample app!",
+        "status": "success"
+    }
+    return JsonResponse(data, safe=False)
