a3ba55dfb1758a29c3cf01dfb1ae77bed990c010
David Dewanto <david.dewanto@gmail.com>
2024-11-08 20:00:40 +0700

    Update Env Variables

--- UTS/settings.py
+++ UTS/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aczr6+7f+37d@md=5&=f$yz7=*vrzk!a%8*5oq13p^-5cmz#$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
