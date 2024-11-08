cd4b192e80c732001886feca059c6a580ad04fb6
David Dewanto <david.dewanto@gmail.com>
2024-11-08 21:00:05 +0700

    Update

--- UTS/settings.py
+++ UTS/settings.py
@@ -130,3 +130,32 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
   '127.0.0.1',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # Temporarily for testing
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+# Also, add your Vercel backend domain to ALLOWED_HOSTS
+ALLOWED_HOSTS = [
+    'uts-david-dewanto-back-end.vercel.app',
+    '127.0.0.1',
+    'localhost'
+]
