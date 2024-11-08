8ff597d38300bf0600841cea4ea20f6345b58f51
David Dewanto <david.dewanto@gmail.com>
2024-11-08 21:16:07 +0700

    update

--- UTS/settings.py
+++ UTS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.vercel.app',  # Allow all Vercel subdomains
+    'uts-david-dewanto-back-end.vercel.app',
+    'uts-david-dewanto-back-hvhp6bdbg-david-dewantos-projects.vercel.app',
+    '.vercel.app',  # This will allow all vercel.app subdomains
     'localhost',
     '127.0.0.1'
 ]
