78cf8f05450d659461fe3664a6e1e10b95fddbd3
David Dewanto <david.dewanto@gmail.com>
2024-11-08 21:48:47 +0700

    update

--- UTS/settings.py
+++ UTS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-development-key')
+SECRET_KEY = 'django-insecure-aczr6+7f+37d@md=5&=f$yz7=*vrzk!a%8*5oq13p^-5cmz#$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
