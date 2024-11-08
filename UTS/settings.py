63deeb70afa3691364543cd5b489f4fa880d2e69
David Dewanto <david.dewanto@gmail.com>
2024-11-08 21:44:21 +0700

    user

--- UTS/settings.py
+++ UTS/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sample',
+    'user',
     'debug_toolbar',
     'corsheaders'
 ]
